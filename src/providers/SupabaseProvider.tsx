f0d6b2c768f55f14f5be0b0054ba5202fbb9d3d5
bikramjeetsarmah03 <9854bikram@gmail.com>
2023-09-29 00:03:32 +0530

    Providers

---
+++ src/providers/SupabaseProvider.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { useState } from "react";
+
+import { Database } from "@/utils/types_db";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { SessionContextProvider } from "@supabase/auth-helpers-react";
+
+interface SupabaseProviderProps {
+  children: React.ReactNode;
+}
+
+export default function SupabaseProvider({ children }: SupabaseProviderProps) {
+  const [supabaseClient] = useState(() =>
+    createClientComponentClient<Database>()
+  );
+
+  return (
+    <SessionContextProvider supabaseClient={supabaseClient}>
+      {children}
+    </SessionContextProvider>
+  );
+}
