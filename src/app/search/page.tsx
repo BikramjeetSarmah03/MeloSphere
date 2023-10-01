20b75dff09cbeb648080aa74817e724fac493827
bikramjeetsarmah03 <9854bikram@gmail.com>
2023-10-01 20:15:45 +0530

    Search Songs

---
+++ src/app/search/page.tsx
@@ -0,0 +1,28 @@
+import getSongsByTitle from "@/actions/getSongsByTitle";
+import SearchContent from "@/components/SearchContent";
+import SearchInput from "@/components/common/SearchInput";
+import Header from "@/components/layout/Header";
+
+interface SearchProps {
+  searchParams: {
+    title: string;
+  };
+}
+
+export default async function Search({ searchParams }: SearchProps) {
+  const songs = await getSongsByTitle(searchParams.title);
+
+  return (
+    <div className="bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto">
+      <Header className="from-neutral-900">
+        <div className="mb-2 flex flex-col gap-y-6">
+          <h1 className="text-white text-3xl font-semibold">Search</h1>
+        </div>
+
+        <SearchInput />
+      </Header>
+
+      <SearchContent songs={songs} />
+    </div>
+  );
+}
---
+++ src/components/SearchContent.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { Song } from "@/utils/types";
+import MediaItem from "./MediaItem";
+
+type Props = {
+  songs: Song[];
+};
+
+export default function SearchContent({ songs }: Props) {
+  if (songs.length === 0) {
+    return (
+      <div className="flex flex-col gap-y-2 w-full px-6 text-neutral-400">
+        No Songs Found
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex flex-col gap-y-2 w-full px-6">
+      {songs.map((song) => (
+        <div className="flex items-center gap-x-4 w-full" key={song.id}>
+          <div className="flex-1">
+            <MediaItem data={song} onClick={() => {}} />
+          </div>
+
+          {/* TODO: Add like button here */}
+        </div>
+      ))}
+    </div>
+  );
+}
